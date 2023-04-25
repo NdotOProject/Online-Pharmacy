a29c8c5e24c12fa730ab8f9ff3a913b0c5a9a0e7
Vũ Tuấn Anh <104408503+tuananhvu1234@users.noreply.github.com>
2023-04-25 17:37:47 +0700

    single page application layout (#2)
    * Connect to server
    
    create RestClient.cs and connect client project to server project
    
    * Create layout
    
    create single page application layout

---
+++ Online_Pharmacy__Client/Controllers/LayoutController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Online_Pharmacy__Client.Controllers
+{
+    public class LayoutController : Controller
+    {
+        public ActionResult GetHeader()
+        {
+            return PartialView("~/Views/Shared/_HeaderPartialPage.cshtml");
+        }
+
+        public ActionResult GetLeftMenu()
+        {
+            return PartialView("~/Views/Shared/_LeftMenuPartialPage.cshtml");
+        }
+        public ActionResult GetRightBar()
+        {
+            return PartialView("~/Views/Shared/_RightBarPartialPage.cshtml");
+        }
+
+        public ActionResult GetFooter()
+        {
+            return PartialView("~/Views/Shared/_FooterPartialPage.cshtml");
+        }
+
+    }
+}
---
+++ Online_Pharmacy__Client/Scripts/layout-controller.js
@@ -0,0 +1,9 @@
+﻿async function LoadAndSetPageInto(id, url) {
+    let page = await fetch(url);
+    let content = await page.text();
+    document.getElementById(id).innerHTML = content;
+}
+
+
+
+
